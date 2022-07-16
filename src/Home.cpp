51f66a81a5651cf9363c64f3815fcbd221d49d51
Jorge Augusto <jorge.lima2407@gmail.com>
2022-07-16 17:52:18 -0300

    Comentado a fu[[

--- src/Home.cpp
+++ src/Home.cpp
@@ -109,13 +109,17 @@ User::Email* User::Home::find(int ID){
  * @return User::Email* | o email que foi apagado
  */
 User::Email* User::Home::erase(int ID){
+    // variável para informar se o último passo que ocorreu foi para a esquerda ou para a direita
     bool step;
 
+    // variáveis para realizar o caminhamento sobre a árvore
     User::Node* previous = nullptr;
     User::Node* current = root;
 
+    // realiza o caminhamento sobre a árvore
     while(current != nullptr){
         if(current->email->id == ID){
+            // se o nó desejado foi encontrado, continua o programa
             break;
         } else if(ID < current->email->id){
             // procura-se na subárvore a esquerda do nó atual, caso o nó armazene o email desejado
@@ -130,46 +134,47 @@ User::Email* User::Home::erase(int ID){
         }
     }
 
+    // verifica se o nó desejado foi encontrado ou se ele não existe
     if(current == nullptr) return nullptr;
 
+    // remoção no caso do nó desejado ser a raiz
     if(current == root){
+        // as duas primeiras condições avaliam se a árvore tem 1 ou nenhum filho, enquanto a última trata quando a árvore tem dois filhos
         if(current->left == nullptr){
             root = current->right;
         } else if(current->right == nullptr){
             root = current->left;
         } else {
+            // variáveis para caminhar pela árvore e achar o adjascente da raíz
             User::Node* adjascente;
             User::Node* adjascentePrevious;
             adjascentePrevious = current;
+
+            // opera na subárvore da direita 
             adjascente = current->right;
             
+            // encontra o nó mais a esquerda da subárvore da direita
             while(adjascente->left != nullptr){
                 adjascentePrevious = adjascente;
                 adjascente = adjascente->left;
             }
 
+            // remove o nó adascente da sua posição
             if(adjascente->right != nullptr){
                 adjascentePrevious->left = adjascente->right;
             } else {
                 adjascentePrevious->left = nullptr;
             }
 
-            if(step == RIGHT){
-                previous->right = adjascente;
-            } else {
-                previous->left = adjascente;
-            }
+            // substitui a ráiz pelo seu adjascente
+            root = adjascente;
 
-            adjascente->right = current->right;
-            adjascente->left = current->left;
-
-            User::Email* aux = current->email;
-            current->email = nullptr;
-            current->right = nullptr;
-            delete current;
-            return aux;
+            // liga a nova raíz aos filhos da raíz anterior
+            root->right = current->right;
+            root->left = current->left;
         }
 
+        // desaloca memória da ráiz anterior
         User::Email* aux = current->email;
         current->email = nullptr;
         current->right = nullptr;
@@ -178,67 +183,88 @@ User::Email* User::Home::erase(int ID){
         return aux;
     }
 
+    // caso de o nó a ser removido ser uma folha
     if(current->left == nullptr && current->right == nullptr){
+        // apenas remove o nó
         if(step == RIGHT){
             previous->right = nullptr;
         } else {
             previous->left = nullptr;
         }
 
+        // destroi o nó
         User::Email* aux = current->email;
         current->email = nullptr;
         delete current;
         return aux;
-    } else if(current->left != nullptr && current->right == nullptr){
+    }
+    // caso de possuir somente um nó a esquerda 
+    else if(current->left != nullptr && current->right == nullptr){
+        // remove o nó, ligando seu sucessor com seu predecessor
         if(step == RIGHT){
             previous->right = current->left;
         } else {
             previous->left = current->left;
         }
 
+        // destroi o nó
         User::Email* aux = current->email;
         current->email = nullptr;
         current->left = nullptr;
         delete current;
         return aux;
-    } else if(current->left == nullptr && current->right != nullptr){
+    } 
+    // caso de possuir somente um nó a direita
+    else if(current->left == nullptr && current->right != nullptr){
+        // remove o nó, ligando seu sucessor com seu predecessor
         if(step == RIGHT){
             previous->right = current->right;
         } else {
             previous->left = current->right;
         }
-
+        
+        // destroi o nó
         User::Email* aux = current->email;
         current->email = nullptr;
         current->right = nullptr;
         delete current;
         return aux;
-    } else {
+    } 
+    // caso de possuir dois filhos
+    else {
+        // variáveis para caminhar pela árvore e achar o adjascente da raíz
         User::Node* adjascente;
         User::Node* adjascentePrevious;
         adjascentePrevious = current;
+
+        // opera na subárvore da direita
         adjascente = current->right;
         
+        // encontra o nó mais a esquerda da subárvore da direita
         while(adjascente->left != nullptr){
             adjascentePrevious = adjascente;
             adjascente = adjascente->left;
         }
 
+        // remove o nó adascente da sua posição
         if(adjascente->right != nullptr){
             adjascentePrevious->left = adjascente->right;
         } else {
             adjascentePrevious->left = nullptr;
         }
-
+        
+        // liga o nó a ser inserido no lugar que deve ser inserido
         if(step == RIGHT){
             previous->right = adjascente;
         } else {
             previous->left = adjascente;
         }
 
+        // liga o nó a ser inserido nos filhos do que acaba de ser removido
         adjascente->right = current->right;
         adjascente->left = current->left;
 
+        // destroi o nó
         User::Email* aux = current->email;
         current->email = nullptr;
         current->right = nullptr;
